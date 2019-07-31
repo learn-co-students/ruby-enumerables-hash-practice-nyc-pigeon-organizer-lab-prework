47a77657822b793de8bfca1210689521d02109f9
Joshua Hill <josh@jlhworkshop.com>
2019-07-31 23:02:52 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  myHash = {}
+  data.each do |key, value|
+    value.each do |newValue, names|
+      names.each do |name|
+        if !myHash[name]
+          myHash[name] = {}
+        end
+        
+        if !myHash[name][key]
+          myHash[name][key] = []
+        end
+        
+        myHash[name][key] << newValue.to_s
+      end
+    end
+  end
+  myHash
 end
