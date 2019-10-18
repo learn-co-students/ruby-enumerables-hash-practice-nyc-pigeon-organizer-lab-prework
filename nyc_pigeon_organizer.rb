e10157b3fb0cbe42faed58cf450f55289e549403
Mubarak Kareem <nelsonsmf@gmail.com>
2019-10-18 07:55:00 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = {}
+  data.each do |key, specs|
+    specs.each do |value, names|
+      names.each do |name|
+      if not pigeons.has_key?(name)
+        pigeons[name] = {}
+      end
+
+      if not pigeons[name].has_key?(key)
+        pigeons[name][key] = []
+      end
+
+      pigeons[name][key] << value.to_s
+    end
+  end
+end
+pigeons
 end
