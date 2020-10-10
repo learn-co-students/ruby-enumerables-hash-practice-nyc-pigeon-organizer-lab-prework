1c15b1ba80d0bd5e9b64cff4cec15c33371d6a6b
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-10-10 22:12:22 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |key, value|
+    value.each do |key2, value2|
+      binding.pry
+      pigeon_list[value2] = {}
+    end
+  end
+  return pigeon_list
 end
