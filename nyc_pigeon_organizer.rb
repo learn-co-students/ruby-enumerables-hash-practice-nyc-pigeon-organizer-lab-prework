db8d74c279a5df3baefd26e9d854c67a06c03eb2
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-10-12 17:27:10 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,17 @@
-require 'pry'
-
 def nyc_pigeon_organizer(data)
-  final_results = data.each_with_object({}) do |(key, value), pigeon_list|
+  pigeon_list = {}
+  data.each do |key, value|
     value.each do |inner_key, names|
       names.each do |name|
         if !pigeon_list[name]
-        pigeon_list[name] = {}
+          pigeon_list[name] = {}
         end
         if !pigeon_list[name][key]
-        pigeon_list[name][key] = []
+          pigeon_list[name][key] = []
         end
         pigeon_list[name][key] << inner_key.to_s
       end
     end
   end
+  return pigeon_list
 end
